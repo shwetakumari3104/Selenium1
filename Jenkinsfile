d04c06700f978f0f7e62da29b195448471faef7d
shwetakumari3104 <shwetakumari3104@gmail.com>
2026-06-17 16:33:01 +0530

    First Commit

---
+++ Jenkinsfile
@@ -0,0 +1,31 @@
+pipeline{
+agent any
+{
+tools{
+maven 'Maven'
+}
+stages{
+stage('Checkout')
+{
+steps
+{
+'git branch:'master', url:"https://www.github.com/Shweta311204/Selenium1.git";
+}}
+stage('Build')
+{
+steps{
+sh 'mvn clean package'
+}}
+stage('Test')
+{
+steps
+{
+sh 'mvn test'
+}}
+stage('Run')
+{
+steps
+{
+sh 'mvn exec:java -Dexec.mainClass="com.example.App"'
+}}}
+}
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+public class App 
+{
+    public static void main( String[] args )
+    {
+    ChromeOptions options=new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--remote-allow-origins=*/");
+    
+    WebDriver driver=new ChromeDriver(options);
+    driver.get("https://www.saucedemo.com/");
+    driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    driver.findElement(By.id("password")).sendKeys("secret_sauce");
+    driver.findElement(By.id("login-button")).click();
+    driver.quit();
+        }
+}
