071449d96be6cc3af65d5d62c3e2d1fdc057d056
shwetakumari3104 <shwetakumari3104@gmail.com>
2026-06-17 17:12:24 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
     ChromeOptions options=new ChromeOptions();
+    options.addArguments("--headless");    
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
     options.addArguments("--remote-allow-origins=*/");
